9459aeb0f5c70c2552a8c8550679fe6e89ccbb76
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2024-08-21 19:19:19 +0530

    🍵WEEK3: JAVA Questions

---
+++ src/WEEK3/Main.java
@@ -0,0 +1,35 @@
+package WEEK3;
+
+class Car {
+    String model;
+    int year;
+    String color;
+
+    // Constructor
+    Car(String model, int year, String color) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+    // Method to display car information
+    void displayCarInfo() {
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Color: " + color);
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating two Car objects
+        Car car1 = new Car("Tesla Model S", 2022, "Red");
+        Car car2 = new Car("Ford Mustang", 2021, "Blue");
+
+        // Displaying their information
+        car1.displayCarInfo();
+        car2.displayCarInfo();
+    }
+}
+
---
+++ src/WEEK3/Rain.java
@@ -0,0 +1,37 @@
+package WEEK3;
+
+class Rectangle {
+    double width;
+    double height;
+
+    // Constructor
+    Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    // Method to calculate the area
+    double calculateArea() {
+        return width * height;
+    }
+
+    // Method to calculate the perimeter
+    double calculatePerimeter() {
+        return 2 * (width + height);
+    }
+}
+
+public class Rain {
+    public static void main(String[] args) {
+        // Creating a Rectangle object
+        Rectangle rectangle = new Rectangle(5.0, 3.0);
+
+        // Calculating and printing the area and perimeter
+        double area = rectangle.calculateArea();
+        double perimeter = rectangle.calculatePerimeter();
+
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+    }
+}
+
